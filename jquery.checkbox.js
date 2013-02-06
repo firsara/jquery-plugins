de3b4711fe08b48a4227bf8a70d51db80be5dc6b
Fabian Irsara <info@fabianirsara.com>
2013-02-06 11:45:52 +0100

    allow automatic deselection of grouped checkboxes (grouped through name)

--- jquery.checkbox.js
+++ jquery.checkbox.js
@@ -3,10 +3,19 @@
   $.fn.checkbox = function(){
     $(this).each(function(){
       var self = $(this);
-      var $cb = $('<span class="checkbox">&nbsp;</span>');
+      var $cb = $('<span class="checkbox dynamic-input">&nbsp;</span>');
       
       var toggleCheckbox = function(){
         $cb.toggleClass('active');
+
+        if (self.attr('data-radio')) {
+          $('input[name="'+self.attr('name')+'"]:checked').not(self).each(function(){
+            $(this).next('.dynamic-input').removeClass('active');
+            $(this).trigger('click');
+            $(this).trigger('change');
+          });
+        }
+
         if ($(this).is('label')) return;
         
         if ($cb.is('.active')) {
